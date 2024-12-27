af8a57e7716956ee43fabb9486de563263ef3129
Gyeony95 <rlg1133@naver.com>
2024-12-27 17:26:07 +0900

    feat : 저장된 RNA 삭제 추가

--- src/lib/components/stm/SeqLoadDialog.svelte
+++ src/lib/components/stm/SeqLoadDialog.svelte
@@ -23,6 +23,12 @@
         showDialog = false;
         dispatch("close");
     }
+
+    function deleteSequence(index, event) {
+        event.stopPropagation(); // 버튼 클릭이 sequence-item 클릭으로 전파되는 것을 방지
+        savedSequences = savedSequences.filter((_, i) => i !== index);
+        localStorage.setItem("savedRnaSeqs", JSON.stringify(savedSequences));
+    }
 </script>
 
 {#if showDialog}
@@ -36,19 +42,29 @@
                         No saved sequences found.
                     </p>
                 {:else}
-                    {#each savedSequences as sequence}
-                        <button
-                            type="button"
-                            class="sequence-item btn btn-light w-100 text-start"
-                            on:click={() => handleSelect(sequence.content)}
-                        >
-                            <div class="sequence-title">
-                                {sequence.title || "Untitled"}
-                            </div>
-                            <div class="sequence-content">
-                                {sequence.content}
-                            </div>
-                        </button>
+                    {#each savedSequences as sequence, index}
+                        <div class="sequence-item-wrapper">
+                            <button
+                                type="button"
+                                class="sequence-item btn btn-light w-100 text-start"
+                                on:click={() => handleSelect(sequence.content)}
+                            >
+                                <div class="sequence-title">
+                                    {sequence.title || "Untitled"}
+                                </div>
+                                <div class="sequence-content">
+                                    {sequence.content}
+                                </div>
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-outline-danger delete-btn"
+                                on:click={(e) => deleteSequence(index, e)}
+                                title="Delete sequence"
+                            >
+                                ×
+                            </button>
+                        </div>
                     {/each}
                 {/if}
             </div>
@@ -132,5 +148,25 @@
         display: flex;
         justify-content: flex-end;
         margin-top: 1rem;
+    }
+
+    .sequence-item-wrapper {
+        position: relative;
+        display: flex;
+        align-items: stretch;
+        margin-bottom: 0.5rem;
+    }
+
+    .sequence-item {
+        margin-bottom: 0;
+        flex-grow: 1;
+    }
+
+    .delete-btn {
+        margin-left: 0.5rem;
+        padding: 0 0.75rem;
+        font-size: 1.5rem;
+        line-height: 1;
+        align-self: stretch;
     }
 </style>
